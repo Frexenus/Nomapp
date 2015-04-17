af721f63e754f995d1d046f8f844c3fb58b420bd
nickolai rovdo <nichogaus@gmail.com>
2015-04-17 17:13:22 +0300

    Tab_content+redesign

--- app/src/main/java/me/insdev/nomapp_show/third_recepie.java
+++ app/src/main/java/me/insdev/nomapp_show/third_recepie.java
@@ -4,14 +4,40 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TabHost;
 
 
 public class third_recepie extends Activity {
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_third_recepie);
+
+        TabHost tabs = (TabHost) findViewById(android.R.id.tabhost);
+
+        tabs.setup();
+
+        TabHost.TabSpec spec = tabs.newTabSpec("tag1");
+
+        spec.setContent(R.id.tab1);
+        spec.setIndicator("Кот");
+        tabs.addTab(spec);
+
+        spec = tabs.newTabSpec("tag2");
+        spec.setContent(R.id.tab2);
+        spec.setIndicator("Кошка");
+        tabs.addTab(spec);
+
+        spec = tabs.newTabSpec("tag3");
+        spec.setContent(R.id.tab3);
+        spec.setIndicator("Котёнок");
+        tabs.addTab(spec);
+
+        tabs.setCurrentTab(0);
+
     }
 
 
