91fec53eff1661161f7b8a22aacbdc63aa86bf1a
nickolai rovdo <nichogaus@gmail.com>
2015-04-20 22:30:35 +0300

    Work with tabs(no content)
    and coming soon features

--- app/src/main/java/me/insdev/nomapp_show/third_recepie.java
+++ app/src/main/java/me/insdev/nomapp_show/third_recepie.java
@@ -2,22 +2,34 @@ package me.insdev.nomapp_show;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+import me.insdev.nomapp_show.SampleActivityBase;
+import android.support.v4.app.FragmentTransaction;
 
 
+public class third_recepie extends /*Activity*/ SampleActivityBase {
 
 
-public class third_recepie extends Activity {
-
-
-    @Override
+   /* @Override
     protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_third_recepie);
 
 
-    }
+    }*/
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_third_recepie);
 
+        if (savedInstanceState == null) {
+            FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+            SlidingTabsBasicFragment fragment = new SlidingTabsBasicFragment();
+            transaction.replace(R.id.sample_content_fragment, fragment);
+            transaction.commit();
+        }
+    }
 
 
 
